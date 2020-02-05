87e0583a0d55b114a933f82b776b05210da9a2e7
wangqingzh <qingwang7962@gmail.com>
2020-02-05 23:29:48 +0800

    init project

---
+++ chilemeserver/src/main/java/com/wangqing/chilemeserver/ChilemeserverApplication.java
@@ -0,0 +1,13 @@
+package com.wangqing.chilemeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChilemeserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChilemeserverApplication.class, args);
+    }
+
+}
---
+++ chilemeserver/src/test/java/com/wangqing/chilemeserver/ChilemeserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wangqing.chilemeserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChilemeserverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
