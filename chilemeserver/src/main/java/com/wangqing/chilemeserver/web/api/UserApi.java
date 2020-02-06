6fd3d6d9bb907ca4eed318b0e586f6888f8f042c
wangqingzh <qingwang7962@gmail.com>
2020-02-06 21:34:57 +0800

    restfull test

---
+++ chilemeserver/src/main/java/com/wangqing/chilemeserver/web/api/UserApi.java
@@ -0,0 +1,13 @@
+package com.wangqing.chilemeserver.web.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户web接口
+ */
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserApi {
+    
+}
--- chilemeserver/src/test/java/com/wangqing/chilemeserver/ChilemeserverApplicationTests.java
+++ chilemeserver/src/test/java/com/wangqing/chilemeserver/ChilemeserverApplicationTests.java
@@ -2,15 +2,22 @@ package com.wangqing.chilemeserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ChilemeserverApplicationTests {
+    private final static Logger logger = LoggerFactory.getLogger(ChilemeserverApplicationTests.class);
 
     @Test
     public void contextLoads() {
+        logger.info("fdasjlg;rjae;grio");
+        logger.debug(new BCryptPasswordEncoder().encode("123"));
+        System.out.println(new BCryptPasswordEncoder().encode("123"));
     }
 
 }
